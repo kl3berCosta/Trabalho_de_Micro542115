b67db245e5cddacdccb531014e0d4f8dd88e19c6
kl3berCosta <155187445+kl3berCosta@users.noreply.github.com>
2025-01-20 00:41:55 -0300

    run.bash
    executar como run.bash

---
+++ run.bash
@@ -0,0 +1,3 @@
+iverilog -o simv Decoder.v Deslocamentos.v Comparador.v OperacoesLogicas.v SaltosCondicionais.v Outros.v NorNotNand.v VariaveisTemporarias.v ArgFuncaoEValRetorno.v CacheMemory.v Memory.v
+vvp simv
+
